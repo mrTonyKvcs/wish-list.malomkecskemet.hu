b5808e54682f36872fb60e7c590d3ef4f1603c87
tony <attila.kovacs92@gmail.com>
2022-12-12 19:25:43 +0100

    closed wish

--- components/information/InformationItem.tsx
+++ components/information/InformationItem.tsx
@@ -3,7 +3,7 @@ import { Item } from "../../types/information";
 const InformationItem = ({ label, text }: Item) => {
   return (
     <p className="mb-3 text-2xl text-justify uppercase">
-      <strong className="text-black">{label}:</strong>{" "}
+      <strong className="text-slate-500">{label}:</strong>{" "}
       <span className="font-bold text-red-malom">{text}</span>
     </p>
   );
